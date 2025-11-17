9921b75920da7c7c998853e2655a2525cf72bcb9
daniu <163009218+daniafzc@users.noreply.github.com>
2025-11-17 23:47:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    toggleBtn.textContent =
+        document.body.classList.contains("dark") ? "☀️ Light Mode" : "🌙 Dark Mode";
+});
