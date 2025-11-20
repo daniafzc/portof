457d609e23982b29f2decc7b425aecc7b4311760
daniu <163009218+daniafzc@users.noreply.github.com>
2025-11-20 23:51:35 +0700

    script.js

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const toggleBtn = document.getElementById("themeToggle");
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     toggleBtn.textContent =
-        document.body.classList.contains("dark") ? "☀️ Light Mode" : "🌙 Dark Mode";
+        document.body.classList.contains("dark") ? "Light Mode" : "Dark Mode";
 });
+
